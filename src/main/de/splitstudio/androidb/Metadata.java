1dddbb4aa91e5c0951c32a622fdfe218bb189719
Matthias Brandt <mb@blau.de>
2010-10-26 11:48:40 +0200

    don't use reserved sql words instead of qouting them (see README).

--- src/main/de/splitstudio/androidb/Metadata.java
+++ src/main/de/splitstudio/androidb/Metadata.java
@@ -9,34 +9,34 @@ class Metadata extends Table {
 	static final String SQL = "CREATE TABLE IF NOT EXISTS Metadata ( _id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, version INTEGER NOT NULL, table TEXT NOT NULL)";
 
 	@Column(notNull = true)
-	private int version;
+	private int tableVersion;
 
 	@Column(notNull = true)
-	private String table;
+	private String tableName;
 
 	public Metadata(final SQLiteDatabase db) {
 		super(db);
 	}
 
 	public boolean findByName(final String name) {
-		Cursor c = db.query(getTableName(), getColumnNames(), "WHERE table='" + name + "'", null, null, null, null);
+		Cursor c = db.query(getTableName(), getColumnNames(), "tableName='" + name + "'", null, null, null, null);
 		return fillFirst(c);
 	}
 
 	public void setVersion(final int version) {
-		this.version = version;
+		this.tableVersion = version;
 	}
 
-	public int getVersion() {
-		return version;
+	public int getTableVersion() {
+		return tableVersion;
 	}
 
 	public void setTable(final String table) {
-		this.table = table;
+		this.tableName = table;
 	}
 
 	public String getTable() {
-		return table;
+		return tableName;
 	}
 
 }
